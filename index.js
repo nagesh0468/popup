ba774d9ee92b2fd581d10f43763ff6d0a7967521
Nagesh <nagesh0468@gmail.com>
2023-02-16 14:38:40 +0530

    this is popup message

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("popup_button");
+
+const popup = document.getElementById("popup");
+
+console.log(popup);
+
+
+function openPopup() {
+    popup.classList.add("open_popup")
+}
+
+
+
+function closePopup() {
+    popup.classList.remove("open_popup")
+}
+
+
+
